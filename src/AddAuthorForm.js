4bddc44f14e386b92359b544a0b3d1a4a1ffa3d7
Felipe <ftoniolo.aat@gmail.com>
2019-01-31 15:35:59 -0200

    Botão Add Author funcionando

--- src/AddAuthorForm.js
+++ src/AddAuthorForm.js
@@ -6,9 +6,12 @@ class AuthorForm extends React.Component {
         super(props);
         this.state = {
             name: '',
-            imageUrl: ''
+            imageUrl: '',
+            books: []
         };
-        this.onFielsChange = this.onFielsChange.bind(this);
+        this.onFieldChange = this.onFieldChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleAddBook = this.handleAddBook.bind(this);
     }
 
     handleSubmit(event) {
@@ -16,24 +19,37 @@ class AuthorForm extends React.Component {
         this.props.onAddAuthor(this.state);
     }
 
-    onFielsChange(event) {
+    onFieldChange(event) {
         this.setState({
             [event.target.name]: event.target.value
         });
+    }
+
+    handleAddBook(event) {
+            this.setState({
+                books: this.state.books.concat([this.state.bookTemp]),
+                bookTemp: ''
+            });
     }
 
     render() {
         return <form onSubmit={this.handleSubmit}>
         <div className="AddAuthorForm_input">
             <label htmlFor="name">Name</label>
-            <input type="text" name="name" value={this.state.name} onChange={this.onFielsChange}/>
+            <input type="text" name="name" value={this.state.name} onChange={this.onFieldChange}/>
         </div>
         <div className="AddAuthorForm_input">
             <label htmlFor="imageUrl">Image URL</label>
-            <input type="text" name="image Url" value={this.state.imageUrl}/>
+            <input type="text" name="imageUrl" value={this.state.imageUrl} onChange={this.onFieldChange}/>
         </div>
+        <div className="AddAuthorForm_input">
+        {this.state.books.map ((book) => <p key={book}>{book}</p>)}
+        <label htmlFor="bookTemp">Books</label>
+        <input type="text"name="bookTemp" value={ this.state.bookTemp} onChange={this.onFieldChange}/>
+        <input type="button"value="+" onClick={this.handleAddBook} />
+            </div>
         <input type="submit" value="Add"/>
-    </form>
+    </form>;
 
     }
 }
